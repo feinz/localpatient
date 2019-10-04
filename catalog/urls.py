15b056d6156bc4de95a9993ee06302e6d8b47c5d
formidable-love <55986274+formidable-love@users.noreply.github.com>
2019-10-04 12:03:40 +0800

    updated urlpatterns
    1. added urls.py in catalog app
    2. changed time zone to local
    3. added path in urls.py localpatient folder

---
+++ catalog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+#pattern for catalog application
+urlpatterns = [
+
+]
--- localpatient/urls.py
+++ localpatient/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.urls import path
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
+#pattern for localpatient project
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('catalog/', include('catalog.urls')), # Use include() to add paths from the catalog application 
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)), #Add URL maps to redirect the base URL to our application
+] #static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # Use static() to add url mapping to serve static files during development (only)
