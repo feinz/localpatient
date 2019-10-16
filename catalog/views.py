3268429117abf1d186a38ba1df4b767106299f2d
formidable-love <55986274+formidable-love@users.noreply.github.com>
2019-10-16 21:46:45 +0800

    temporary changed the current state of the chart on homepage
    1. textbox to filter year to display patient number on chart

--- catalog/views.py
+++ catalog/views.py
@@ -5,9 +5,10 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView #easily create,update,view form
 from django.urls import reverse_lazy
-
 # importing Patient models
 from catalog.models import Patient
+# importing date time
+# from datetime import datetime
 
 @login_required
 def index(request):
@@ -18,12 +19,34 @@ def index(request):
     patient_act = Patient.objects.filter(patient_status__exact='ACT').count()
     patient_rip = Patient.objects.filter(patient_status__exact='RIP').count()   
     patient_mia = Patient.objects.filter(patient_status__exact='MIA').count()
-    context = {
-        'patient_number': patient_number,
-        'patient_act': patient_act,
-        'patient_rip': patient_rip,
-        'patient_mia': patient_mia,
+
+    #input query to display user input year graph
+    query = request.GET.get('q')
+    if not query:
+            patient_year = Patient.objects.all().dates('time_registered','year')
+            context = {
+            'patient_number': patient_number,
+            'patient_total_act': patient_act,
+            'patient_total_rip': patient_rip,
+            'patient_total_mia': patient_mia,
+            'patient_filter_year': patient_year,
     }
+    else:
+            # display current patient for each of the status
+            patient_yearinput_mia = Patient.objects.filter(Q(time_registered__contains=query)&Q(patient_status__contains="MIA")).count()
+            patient_yearinput_act = Patient.objects.filter(Q(time_registered__contains=query)&Q(patient_status__contains="ACT")).count()
+            patient_yearinput_rip = Patient.objects.filter(Q(time_registered__contains=query)&Q(patient_status__contains="RIP")).count()
+            context = { 
+            'patient_number': patient_number,
+            'patient_total_act': patient_act,
+            'patient_total_rip': patient_rip,
+            'patient_total_mia': patient_mia,
+            'patient_yearinput_mia': patient_yearinput_mia,
+            'patient_yearinput_act': patient_yearinput_act,
+            'patient_yearinput_rip': patient_yearinput_rip,
+    }
+    # patient_time_registered = Patient.objects.filter(time_registered=patient_year)
+
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
     
