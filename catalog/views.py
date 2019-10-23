fabb3e125a66c4a2c1532d0b2395fad37a773bc2
formidable-love <55986274+formidable-love@users.noreply.github.com>
2019-10-23 14:46:14 +0800

    added graph view montly for each year
    1. monthly graph of total patient for each status
    2. adjusted color for each of the graph bar

--- catalog/views.py
+++ catalog/views.py
@@ -25,22 +25,27 @@ def index(request):
     if not query:
             currentyear = 0000
 
+            # for year 2015
             patient_year2015_mia = Patient.objects.filter(Q(time_registered__year=2015)&Q(patient_status__contains="Missing")).count()
             patient_year2015_act = Patient.objects.filter(Q(time_registered__year=2015)&Q(patient_status__contains="Active")).count()
             patient_year2015_rip = Patient.objects.filter(Q(time_registered__year=2015)&Q(patient_status__contains="Rest In Peace")).count()
-
+            
+            # for year 2016
             patient_year2016_mia = Patient.objects.filter(Q(time_registered__year=2016)&Q(patient_status__contains="Missing")).count()
             patient_year2016_act = Patient.objects.filter(Q(time_registered__year=2016)&Q(patient_status__contains="Active")).count()
             patient_year2016_rip = Patient.objects.filter(Q(time_registered__year=2016)&Q(patient_status__contains="Rest In Peace")).count()
 
+            # for year 2017
             patient_year2017_mia = Patient.objects.filter(Q(time_registered__year=2017)&Q(patient_status__contains="Missing")).count()
             patient_year2017_act = Patient.objects.filter(Q(time_registered__year=2017)&Q(patient_status__contains="Active")).count()
             patient_year2017_rip = Patient.objects.filter(Q(time_registered__year=2017)&Q(patient_status__contains="Rest In Peace")).count()
 
+            # for year 2018
             patient_year2018_mia = Patient.objects.filter(Q(time_registered__year=2018)&Q(patient_status__contains="Missing")).count()
             patient_year2018_act = Patient.objects.filter(Q(time_registered__year=2018)&Q(patient_status__contains="Active")).count()
             patient_year2018_rip = Patient.objects.filter(Q(time_registered__year=2018)&Q(patient_status__contains="Rest In Peace")).count()
 
+            # for year 2019
             patient_year2019_mia = Patient.objects.filter(Q(time_registered__year=2019)&Q(patient_status__contains="Missing")).count()
             patient_year2019_act = Patient.objects.filter(Q(time_registered__year=2019)&Q(patient_status__contains="Active")).count()
             patient_year2019_rip = Patient.objects.filter(Q(time_registered__year=2019)&Q(patient_status__contains="Rest In Peace")).count()
@@ -50,9 +55,10 @@ def index(request):
             'patient_total_act': patient_act,
             'patient_total_rip': patient_rip,
             'patient_total_mia': patient_mia,
+
             'patient_year2015_mia': patient_year2015_mia,
             'patient_year2015_act': patient_year2015_act,
-            'patient_year2015_rip': patient_year2015_rip,
+            'patient_year2015_rip': patient_year2015_rip,    
             'patient_year2016_mia': patient_year2016_mia,
             'patient_year2016_act': patient_year2016_act,
             'patient_year2016_rip': patient_year2016_rip,
@@ -68,22 +74,98 @@ def index(request):
             'currentyear': currentyear,
             }
     else:
-            patient_yearinput_mia = Patient.objects.filter(Q(time_registered__contains=query)&Q(patient_status__contains="Missing")).count()
+            patient_total_year = Patient.objects.filter(time_registered__year=query).count()
             # display current patient for each of the status
-            patient_yearinput_mia = Patient.objects.filter(Q(time_registered__contains=query)&Q(patient_status__contains="Missing")).count()
-            patient_yearinput_act = Patient.objects.filter(Q(time_registered__contains=query)&Q(patient_status__contains="Active")).count()
-            patient_yearinput_rip = Patient.objects.filter(Q(time_registered__contains=query)&Q(patient_status__contains="Rest In Peace")).count()
+            patient_yearinput_mia = Patient.objects.filter(Q(time_registered__year=query)&Q(patient_status__contains="Missing")).count()
+            patient_yearinput_act = Patient.objects.filter(Q(time_registered__year=query)&Q(patient_status__contains="Active")).count()
+            patient_yearinput_rip = Patient.objects.filter(Q(time_registered__year=query)&Q(patient_status__contains="Rest In Peace")).count()
             currentyear = query
+
+            #query to get number of patient for each status, each month in selected year from input
+            patient_year_mia_01 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=1)&Q(patient_status__contains="Missing")).count()
+            patient_year_mia_02 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=2)&Q(patient_status__contains="Missing")).count()
+            patient_year_mia_03 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=3)&Q(patient_status__contains="Missing")).count()
+            patient_year_mia_04 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=4)&Q(patient_status__contains="Missing")).count()
+            patient_year_mia_05 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=5)&Q(patient_status__contains="Missing")).count()
+            patient_year_mia_06 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=6)&Q(patient_status__contains="Missing")).count()
+            patient_year_mia_07 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=7)&Q(patient_status__contains="Missing")).count()
+            patient_year_mia_08 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=8)&Q(patient_status__contains="Missing")).count()
+            patient_year_mia_09 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=9)&Q(patient_status__contains="Missing")).count()
+            patient_year_mia_10 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=10)&Q(patient_status__contains="Missing")).count()
+            patient_year_mia_11 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=11)&Q(patient_status__contains="Missing")).count()
+            patient_year_mia_12 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=12)&Q(patient_status__contains="Missing")).count()
+            patient_year_act_01 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=1)&Q(patient_status__contains="Active")).count()
+            patient_year_act_02 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=2)&Q(patient_status__contains="Active")).count()
+            patient_year_act_03 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=3)&Q(patient_status__contains="Active")).count()
+            patient_year_act_04 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=4)&Q(patient_status__contains="Active")).count()
+            patient_year_act_05 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=5)&Q(patient_status__contains="Active")).count()
+            patient_year_act_06 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=6)&Q(patient_status__contains="Active")).count()
+            patient_year_act_07 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=7)&Q(patient_status__contains="Active")).count()
+            patient_year_act_08 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=8)&Q(patient_status__contains="Active")).count()
+            patient_year_act_09 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=9)&Q(patient_status__contains="Active")).count()
+            patient_year_act_10 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=10)&Q(patient_status__contains="Active")).count()
+            patient_year_act_11 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=11)&Q(patient_status__contains="Active")).count()
+            patient_year_act_12 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=12)&Q(patient_status__contains="Active")).count()
+            patient_year_rip_01 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=1)&Q(patient_status__contains="Rest In Peace")).count()
+            patient_year_rip_02 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=2)&Q(patient_status__contains="Rest In Peace")).count()
+            patient_year_rip_03 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=3)&Q(patient_status__contains="Rest In Peace")).count()
+            patient_year_rip_04 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=4)&Q(patient_status__contains="Rest In Peace")).count()
+            patient_year_rip_05 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=5)&Q(patient_status__contains="Rest In Peace")).count()
+            patient_year_rip_06 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=6)&Q(patient_status__contains="Rest In Peace")).count()
+            patient_year_rip_07 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=7)&Q(patient_status__contains="Rest In Peace")).count()
+            patient_year_rip_08 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=8)&Q(patient_status__contains="Rest In Peace")).count()
+            patient_year_rip_09 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=9)&Q(patient_status__contains="Rest In Peace")).count()
+            patient_year_rip_10 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=10)&Q(patient_status__contains="Rest In Peace")).count()
+            patient_year_rip_11 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=11)&Q(patient_status__contains="Rest In Peace")).count()
+            patient_year_rip_12 = Patient.objects.filter(Q(time_registered__year=currentyear)&Q(time_registered__month=12)&Q(patient_status__contains="Rest In Peace")).count()
 
             context = { 
             'patient_number': patient_number,
             'patient_total_act': patient_act,
             'patient_total_rip': patient_rip,
             'patient_total_mia': patient_mia,
+            'patient_total_year': patient_total_year,
             'patient_yearinput_mia': patient_yearinput_mia,
             'patient_yearinput_act': patient_yearinput_act,
             'patient_yearinput_rip': patient_yearinput_rip,
             'currentyear': currentyear,
+            'patient_year_mia_01': patient_year_mia_01,
+            'patient_year_mia_02': patient_year_mia_02,
+            'patient_year_mia_03': patient_year_mia_03,
+            'patient_year_mia_04': patient_year_mia_04,
+            'patient_year_mia_05': patient_year_mia_05,
+            'patient_year_mia_06': patient_year_mia_06,
+            'patient_year_mia_07': patient_year_mia_07,
+            'patient_year_mia_08': patient_year_mia_08,
+            'patient_year_mia_09': patient_year_mia_09,
+            'patient_year_mia_10': patient_year_mia_10,
+            'patient_year_mia_11': patient_year_mia_11,
+            'patient_year_mia_12': patient_year_mia_12,
+            'patient_year_act_01': patient_year_act_01,
+            'patient_year_act_02': patient_year_act_02,
+            'patient_year_act_03': patient_year_act_03,
+            'patient_year_act_04': patient_year_act_04,
+            'patient_year_act_05': patient_year_act_05,
+            'patient_year_act_06': patient_year_act_06,
+            'patient_year_act_07': patient_year_act_07,
+            'patient_year_act_08': patient_year_act_08,
+            'patient_year_act_09': patient_year_act_09,
+            'patient_year_act_10': patient_year_act_10,
+            'patient_year_act_11': patient_year_act_11,
+            'patient_year_act_12': patient_year_act_12,
+            'patient_year_rip_01': patient_year_rip_01,
+            'patient_year_rip_02': patient_year_rip_02,
+            'patient_year_rip_03': patient_year_rip_03,
+            'patient_year_rip_04': patient_year_rip_04,
+            'patient_year_rip_05': patient_year_rip_05,
+            'patient_year_rip_06': patient_year_rip_06,
+            'patient_year_rip_07': patient_year_rip_07,
+            'patient_year_rip_08': patient_year_rip_08,
+            'patient_year_rip_09': patient_year_rip_09,
+            'patient_year_rip_10': patient_year_rip_10,
+            'patient_year_rip_11': patient_year_rip_11,
+            'patient_year_rip_12': patient_year_rip_12,
+
             }
     # patient_time_registered = Patient.objects.filter(time_registered=patient_year)
 
