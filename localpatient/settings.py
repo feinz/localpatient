b017e1ff5d0c703420e716236f07f08422951279
formidable-love <55986274+formidable-love@users.noreply.github.com>
2019-10-27 21:10:52 +0800

    added apps in settings.py

--- localpatient/settings.py
+++ localpatient/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'patientinfo',
+    'appointment',
+    'staffinfo',
+    'startpoint',
 ]
 
 MIDDLEWARE = [
