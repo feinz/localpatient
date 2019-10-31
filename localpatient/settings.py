f80ed977800e92f40aaba289d83e1782aea96c5e
alice <feinzunofficial@gmail.com>
2019-10-31 08:11:45 +0800

    new

--- localpatient/settings.py
+++ localpatient/settings.py
@@ -37,14 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig',
     'startpoint',
     'appointment',
     'patientinfo',
     'staffinfo',
 ]
-
-AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
